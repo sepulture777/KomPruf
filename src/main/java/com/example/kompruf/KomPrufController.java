18914d66bc03c86b8b8c9ef78b199b58a62040fc
Erich <ic19b071@technikum-wien.at>
2023-03-28 18:51:44 +0200

    Test if server responds -> green

--- src/main/java/com/example/kompruf/KomPrufController.java
+++ src/main/java/com/example/kompruf/KomPrufController.java
@@ -1,6 +1,7 @@
 package com.example.kompruf;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //make it a controller all beans are generated automatically
@@ -8,6 +9,11 @@ public class KomPrufController {
 
     @GetMapping("/")    //we generate a mapping to cause a Http200 response
     public void beAvailable(){
+    }
+
+    @GetMapping("bmi")
+    public Double calculateBMI(@RequestParam Double weight, @RequestParam Double height) {
+
     }
 
 
