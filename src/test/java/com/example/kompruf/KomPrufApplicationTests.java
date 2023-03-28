f403cf669b3ee701020bf60dfa14ac496945215d
Erich <ic19b071@technikum-wien.at>
2023-03-28 18:14:53 +0200

    Test if server responds -> red

--- src/test/java/com/example/kompruf/KomPrufApplicationTests.java
+++ src/test/java/com/example/kompruf/KomPrufApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.kompruf;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class KomPrufApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mvc;
+    @Test
+    void isTheServerAvailable() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+    }
 
 }
